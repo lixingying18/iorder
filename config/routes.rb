b8141e7dfaa613ca19683e81307d8e5b2630c3e5
shuailw <shuailongwen@gmail.com>
2018-12-15 03:24:56 +0000

    完成了主页的静态页面, 编写了一些测试文件

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
+  get 'static_pages/home'
+  get 'static_pages/about'
+  get 'static_pages/services'
+  get 'static_pages/menu'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

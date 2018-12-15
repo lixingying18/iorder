f8dd63bab03ff51a3afdbbd7434c1b46f05a5422
shuailw <shuailongwen@gmail.com>
2018-12-15 06:10:36 +0000

    新建了User模型

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root "static_pages#home"
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/services'
-  get 'static_pages/menu'
+  # get 'static_pages/home'
+  # get 'static_pages/about'
+  # get 'static_pages/services'
+  # get 'static_pages/menu'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get '/about', to: 'static_pages#about'
+  get '/menu', to: 'static_pages#menu'
+  get '/home', to: 'static_pages#home'
+  get '/service', to: 'static_pages#service'
+  get '/signup', to: 'users#new'
 end
